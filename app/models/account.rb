819afbd9836107bc6c429042f225c7e7b76272d1
Fares <m.fares.harrazi@gmail.com>
2024-11-25 21:33:50 +0200

    add update balance and balance transfer logic

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,15 @@ class Account < ApplicationRecord
   has_many :transactions
 
   validates :percentage, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
+
+  def transfer_to(other_account, amount)
+    return if self.balance < amount
+
+    self.transactions.create!(name: "Transfer to #{other_account.title}", amount: -amount, category: nil)
+    self.update!(balance: self.balance - amount)
+
+    other_account.transactions.create!(name: "Transfer from #{self.title}", amount: amount, category: nil)
+    other_account.update!(balance: other_account.balance + amount)
+  end
+
 end
--- app/models/general_income.rb
+++ app/models/general_income.rb
@@ -1,4 +1,12 @@
 class GeneralIncome < ApplicationRecord
+  after_create :allocate_to_accounts
   belongs_to :general_account
   validates :title, :amount, presence: true
+
+  def allocate_to_accounts
+  general_account.accounts.each do |account|
+    account.update(balance: account.balance + (amount * account.percentage / 100))
+  end
+  general_account.calculate_net_income
+end
 end
