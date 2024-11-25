6235611def160aaeba92d9f78fb498e625d27fb9
Fares <m.fares.harrazi@gmail.com>
2024-11-25 21:27:18 +0200

    update account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,7 @@
 class Account < ApplicationRecord
   belongs_to :general_account
+  has_many :categories
+  has_many :transactions
+
+  validates :percentage, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
 end
