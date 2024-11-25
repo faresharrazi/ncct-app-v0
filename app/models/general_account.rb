444393b226f094d80689620ed5f9470fc27aeb28
Fares <m.fares.harrazi@gmail.com>
2024-11-25 21:25:33 +0200

    update general account model

--- app/models/general_account.rb
+++ app/models/general_account.rb
@@ -1,2 +1,10 @@
 class GeneralAccount < ApplicationRecord
+  has_many :general_incomes
+  has_many :general_expenses
+  has_many :accounts
+
+  def calculate_net_income
+    self.net_income = general_incomes.sum(:amount) - general_expenses.sum(:amount)
+    save
+  end
 end
